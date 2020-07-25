8640f758d2ef3f59bbbcf22ce27aa486a78de636
sswellington <wellingtons@id.uff.br>
2020-07-24 23:46:15 -0300

    refactor: listen net

--- ListenNet.py
+++ ListenNet.py
@@ -1,19 +1,31 @@
 import json
 import speedtest
 
-class ListenNet(object):
-    listen = None
 
-    def __init__(self):
-        self.listen = speedtest.Speedtest() # line 1073
-    
-    def info(self):
-        self.listen.get_servers([])
+class ListenNet(object):
+    info: None
+    ping: float
+    upload: float
+    download: float
+    listen = speedtest.Speedtest() 
+
+    def __init__(self, set_server=[]):
+        """ Arrumar a interface """
+
+
+        self.listen = speedtest.Speedtest()  # line 1073
+        print("Testing from Y")
+        print("Connect Server.............................")
+        self.listen.get_servers(set_server)
+        print("Selecting best server based on ping........")
         self.listen.get_best_server()
-        # in bit
-        self.listen.download() 
-        self.listen.upload()
-        """ return {
+        # print("Hosted by ", (hosted['Sponsor']))
+        self.ping = self.listen.results.ping
+        """ in bit """
+        self.download = self.listen.download()
+        self.upload = self.listen.upload()
+        """ info
+            Return dictionary of result data {
             'download': self.listen.results.download,
             'upload': self.listen.results.upload,
             'ping': self.listen.results.ping,
@@ -24,4 +36,16 @@ class ListenNet(object):
             'share': self.listen.results._share,
             'client': self.listen.results.client,
         } """
-        return (self.listen.results.dict)
+        self.info = self.listen.results.dict()
+
+    def get_url(self):
+        """POST data to the speedtest.net API to obtain a share results link"""
+        return self.listen.results.share()
+
+    def report(self):
+        """ Doc: Use the lib `json` - RFC 7159
+		    [Source](https://docs.python.org/3/library/json.html?highlight=json#module-json)
+        """
+        json_object = json.dumps(self.info, indent = 4) 
+        with open("report.json", "w") as outfile: 
+            outfile.write(json_object) 
--- main.py
+++ main.py
@@ -3,8 +3,12 @@ from ListenNet import ListenNet
 if __name__ == '__main__':
     print("begin") # development variable
     try:
-        t = ListenNet()
-        print(t.info())
-    except Error:
-        print("Error 935: ListenNet")
+        t = ListenNet([22754])
+        print(t.info)
+        # t.report()
+        print("ping:", t.ping)
+        print("upload:", t.upload)
+        print("download:", t.download)
+    except ListenNet():
+        raise SystemExit("Error 935: ListenNet")
 print("end") # development variable
